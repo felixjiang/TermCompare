09ff1ea35f17b90943908378375f0a867cc4d43e
felixjiang <felixmail@vip.163.com>
2020-08-20 15:41:08 +0800

    Add files via upload

--- TCQuery.sql
+++ TCQuery.sql
@@ -6,7 +6,8 @@ WHERE sentence IN ('With Windows Hello as shown in the following figure, you can
 'SRXEditor includes a sample file in SRX 2.0 format with a default set of segmentation rules supporting most standard cases as follows.',
 'In this page, it also includes segmentation rules specific for these languages.',
 'For some programming scenarios in Office Add-ins that use one of the host-specific API models (for Unix, Excel, Word, OneNote, and Visio).',
-'Your code needs to read, write, or process some property on the dialog box from every member of a collection object.'
+'Your code needs to read, write, or process some property on the dialog box from every member of a collection object.',
+'With Windows Hello on Unix as shown in the following figure, you can accomplish a lot of things in self-built Unix.'
 );
 
 DELETE log
@@ -15,6 +16,18 @@ FROM log INNER JOIN (
 	GROUP BY sentence, rule_hit
 	HAVING COUNT(rule_hit) > 1) b
 WHERE log.sentence = b.sentence AND log.rule_hit=b.rule_hit AND log.id <> b.id;
+
+UPDATE log
+SET user_guid = NULL
+WHERE user_guid = '';
+
+UPDATE log
+SET ip_location = NULL
+WHERE ip_location = '';
+
+UPDATE log
+SET session_guid = NULL
+WHERE session_guid = '';
 
 SELECT a.WEEK AS week, unique_users, sessions, runs, total_issues, false_pos, CONCAT(ROUND(false_pos/total_issues*100,2),'%') AS false_pos_rate FROM
 (SELECT datediff(check_date,'2020-06-26') DIV 7 + 1 AS week, COUNT(sentence) AS total_issues, COUNT(DISTINCT user_guid) AS unique_users, COUNT(DISTINCT LEFT(session_guid, 37)) as sessions FROM log
@@ -44,10 +57,11 @@ sentence VARCHAR(2000),
 rule_hit SMALLINT UNSIGNED,
 feedback VARCHAR(50),
 ip_location VARCHAR(50),
-user_guid VARCHAR(50));
+user_guid VARCHAR(50),
+session_guid VARCHAR(50));
 
 INSERT INTO log_bak
-SELECT check_date, sentence, rule_hit, feedback, ip_location, user_guid
+SELECT check_date, sentence, rule_hit, feedback, ip_location, user_guid, session_guid
 FROM log
 ORDER BY id;
 
@@ -60,10 +74,13 @@ CREATE TABLE log (
 check_date DATETIME,
 sentence VARCHAR(2000),
 rule_hit SMALLINT UNSIGNED,
-feedback VARCHAR(50));
+feedback VARCHAR(50),
+ip_location VARCHAR(50),
+user_guid VARCHAR(50),
+session_guid VARCHAR(50));
 
 INSERT INTO log
-SELECT check_date, sentence, rule_hit, feedback
+SELECT check_date, sentence, rule_hit, feedback, ip_location, user_guid, session_guid
 FROM log_bak
 ORDER BY id;
 
